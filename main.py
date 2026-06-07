efe8d263debb77044f033753567efdcd8bcb9509
Ambati Yashwanth <yashwanth.ambati321@gmail.com>
2026-06-07 13:42:06 +0530

    updated

---
+++ main.py
@@ -0,0 +1,72 @@
+from fastapi import FastAPI, Query
+from langchain.agents import create_agent
+from langchain_groq import ChatGroq
+from langchain.tools import tool
+from dotenv import load_dotenv
+import requests
+import os
+
+load_dotenv()
+
+app = FastAPI()
+
+# API Keys
+
+API_KEY = os.getenv("OPENWEATHER_API_KEY")
+
+print("Weather Key:", API_KEY)
+
+# LLM
+llm = ChatGroq(
+    model="llama-3.3-70b-versatile",
+    api_key=os.getenv("api_key")
+)
+
+# Tool
+@tool 
+def get_temp_details(city:str):
+    """
+    this is to get city details 
+    """
+    # print(type(city))
+    res=requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}")
+
+    data=res.json()
+    return data
+
+# Agent
+agent = create_agent(
+    model=llm,
+    tools=[get_temp_details]
+)
+
+@app.get("/")
+def home():
+    return {
+        "message": "Weather Agent Running"
+    }
+
+
+@app.post("/get_weather")
+def incoming_weather_params(
+    city: str = Query(...),
+    question: str = Query(...)
+):
+
+    result = agent.invoke({
+        "messages": [
+            {
+                "role": "user",
+                "content": f"""
+                City: {city}
+
+                User Question:
+                {question}
+
+                Use the weather tool first and then answer.
+                """
+            }
+        ]
+    })
+
+    return result
